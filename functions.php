a8bd418df1c36f9e90eba14b2dbe5a7c8fe28613
lukman <nandes88.ni@gmail.com>
2021-01-17 18:16:15 +0700

    menambahkan menampilkan seluruh data siswa

--- functions.php
+++ functions.php
@@ -14,3 +14,17 @@ function query($query) {
 function filter($requst) {
   return mysqli_real_escape_string(koneksi(), htmlspecialchars($requst));
 }
+
+function httpRequest($url) {
+  // inisialisasi curl
+  $curlInit = curl_init();
+  // set opsi curl
+  curl_setopt($curlInit, CURLOPT_URL, $url);
+  // return the transfer as a string 
+  curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, 1);
+  $outputCurl = json_decode(curl_exec($curlInit), true);
+
+  // tutup curl
+  curl_close($curlInit);
+  return $outputCurl;
+}
--- views/index.php
+++ views/index.php
@@ -2,7 +2,7 @@
 
 require_once '../functions.php';
 
-var_dump($getAllDataSiswa);
+$getAllDataSiswa = httpRequest('http://localhost/data-siswa/siswa.php');
 ?>
 
 <!DOCTYPE html>
@@ -18,11 +18,33 @@ var_dump($getAllDataSiswa);
 <body>
   <h1 class="text-center text-4xl mt-9">Data Siswa</h1>
   <div class="container">
-    <ul class="ml-10 bg-blue-400 text-white">
-      <li>
-        Nama: 
+  <?php foreach($getAllDataSiswa['data'] as $dataSiswa) : ?>
+    <ul class="m-4 border border-solid border-indigo-400 rounded w-7/12 mx-auto bg-indigo-200 divide-opacity-75">
+      <li class="ml-5 my-3">
+      Nama: <p class="font-semibold inline text-indigo-500">
+              <?= $dataSiswa['nama']; ?>
+            </p>
+      </li>
+      <li class="m-3 ml-5">
+        Jurusan: <p class="font-semibold inline text-indigo-500">
+                  <?= $dataSiswa['jurusan']; ?>
+                </p>
+      </li>
+      <li class="m-3 ml-5">
+        Aksi:
+        <a 
+          href="detail.php?id=<?= $dataSiswa['id']; ?>" 
+          class="text-white bg-blue-400 p-2 rounded">
+          Detail
+        </a>
+        <a 
+          href="detail.php?id=<?= $dataSiswa['id']; ?>" 
+          class="text-white bg-blue-600 ml-2 p-2 rounded">
+          Update
+        </a>
       </li>
     </ul>
+    <?php endforeach; ?>
   </div>
 </body>
 
